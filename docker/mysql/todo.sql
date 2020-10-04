e92b7370a26e90334e550c4cdcb950c014a43d77
mwayi <mr.mwayi@gmail.com>
2020-10-04 18:00:09 +0100

    Update pip requirements and initial sql to add more fixtures

--- docker/mysql/todo.sql
+++ docker/mysql/todo.sql
@@ -14,4 +14,7 @@ CREATE TABLE `todos` (
 
 INSERT INTO `todos` (`id`, `created`, `description`, `uuid`, `status`, `deleted`)
 VALUES
-	(1, '2020-08-31 16:57:33', 'Take out the milk', '3ac66762-fb8e-11ea-adc1-0242ac120002', '', NULL);
+	(1, '2020-08-31 16:57:33', 'Make a todo list', '3ac66762-fb8e-11ea-adc1-0242ac120002', 'done', NULL),
+	(2, '2020-09-21 13:35:04', 'Check off first thing on todo list', '55009123-9bb3-4992-87c1-f8bd4eff0b87', 'done', NULL),
+	(3, '2020-09-21 13:35:28', 'Realise that I have already done two things', 'b356940e-9a30-481e-b62e-886c0dd75793', NULL, NULL),
+	(5, '2020-09-21 13:51:22', 'Reward myself for the above accomplishments', '736e435d-00ea-484a-8823-55129a157161', NULL, NULL);
